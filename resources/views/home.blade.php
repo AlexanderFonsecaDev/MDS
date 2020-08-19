c34ec20f374804631b467e2a548b8d1edbeb8a67
Dante <jhonalexanderfonseca1993@gmail.com>
2020-08-19 04:27:28 -0500

    arreglo de la tabla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                 </div>
 
                 <div class="card-body">
-                    <table class="table">
+                    <table class="table table-responsive">
                         <thead class="thead-dark">
                         <tr>
                             <th scope="col">ID</th>
